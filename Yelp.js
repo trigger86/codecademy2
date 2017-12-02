0015c1c5b4936198aafddc15395cac4505539252
trigger86 <olegg86reg@gmail.com>
2017-12-02 23:26:50 +0600

    Create Yelp.js

---
+++ Yelp.js
@@ -0,0 +1,45 @@
+const clientId = '7avScoUTuqK-e1qpmaQWQw';
+const secret = 'N2f1QjrvmWHjenklS8ixXB9oqblq4CisFpBCFgHZlcko3bFDhAkhdHAXKgiGo37Y';
+let accessToken;
+const Yelp = {
+
+	getAccessToken(){
+		if(accessToken){
+			return new Promise(resolve => resolve(accessToken));
+		}
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {method: 'POST'}).then(
+			response => response.json()).then(
+			jsonResponse=> accessToken = jsonResponse.access_token)
+        },
+
+    search(term,location,sortBy){
+          return Yelp.getAccessToken().then(()=>{
+          	return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+          		headers: {	}
+          	})
+          }).then(jsonResponse=>{
+          	if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business=>{
+                		return {
+                			id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.address,
+                            city: business.city,
+                            state: business.state,
+                            zipCode: business.zipCode,
+                            category: business.category,
+                            rating: business.rating,
+                            reviewCount: business.reviewCount
+                		}
+                });
+               }
+          })
+    }   
+
+
+
+		
+}
+
+export {Yelp}
